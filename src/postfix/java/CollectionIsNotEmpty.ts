e3c98701dc07e53b4ec8a489d703f4e2ff00ef48
chana1024 <linzhinan1024@gmail.com>
2022-08-13 16:42:23 +0800

    inti

--- src/postfix/java/CollectionIsNotEmpty.ts
+++ src/postfix/java/CollectionIsNotEmpty.ts
@@ -4,7 +4,7 @@ import { Return } from "../../base/decorator/Return";
 import { Target } from "../../base/decorator/Target";
 import PostfixHandler from "../../base/suggest/PostfixHandler";
 
-@EnablePostfixSuggestion({ language: "java", label: "Collection.isEmpty" })
+@EnablePostfixSuggestion({ language: "java", label: "Collection.isNotEmpty" })
 class CollectionIsNotEmpty extends PostfixHandler {
     @Target.Slice({})
     @Return.Replace()
